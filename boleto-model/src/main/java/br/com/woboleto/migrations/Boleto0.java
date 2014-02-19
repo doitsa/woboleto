efd10c51b5ace29475e8cc560dc514118d04ed67
Henrique Prange <hprange@gmail.com>
2014-02-18 23:55:01 -0300

    Refatora o nome das tabelas e gera novamente o migrations

--- boleto-model/src/main/java/br/com/woboleto/migrations/Boleto0.java
+++ boleto-model/src/main/java/br/com/woboleto/migrations/Boleto0.java
@@ -7,83 +7,77 @@ import er.extensions.migration.ERXMigrationTable;
 
 public class Boleto0 extends ERXMigrationDatabase.Migration
 {
-	@Override
-	public void downgrade(final EOEditingContext editingContext, final ERXMigrationDatabase database) throws Throwable
-	{
-		// DO NOTHING
-	}
+    @Override
+    public void downgrade(final EOEditingContext editingContext, final ERXMigrationDatabase database) throws Throwable
+    {
+        // DO NOTHING
+    }
 
-	@Override
-	public void upgrade(final EOEditingContext editingContext, final ERXMigrationDatabase database) throws Throwable
-	{
-		ERXMigrationTable sacadoTable = database.newTableNamed("boleto_sacado");
+    @Override
+    public void upgrade(final EOEditingContext editingContext, final ERXMigrationDatabase database) throws Throwable
+    {
+        ERXMigrationTable abstractInformacaoTable = database.newTableNamed("informacao");
+        abstractInformacaoTable.newIntegerColumn("boleto_id", false);
+        abstractInformacaoTable.newIntegerColumn("id", false);
+        abstractInformacaoTable.newStringColumn("tipo", 1, false);
+        abstractInformacaoTable.newStringColumn("valor", 255, false);
+        abstractInformacaoTable.create();
+        abstractInformacaoTable.setPrimaryKey("id");
 
-		sacadoTable.newStringColumn("bairro", 255, true);
-		sacadoTable.newStringColumn("cep", 50, true);
-		sacadoTable.newStringColumn("cidade", 255, true);
-		sacadoTable.newStringColumn("cpf", 50, true);
-		sacadoTable.newStringColumn("endereco", 255, true);
-		sacadoTable.newIntegerColumn("id", false);
-		sacadoTable.newStringColumn("nome", 255, true);
-		sacadoTable.newStringColumn("uf", 2, true);
-		sacadoTable.create();
-		sacadoTable.setPrimaryKey("id");
+        ERXMigrationTable eoBoletoTable = database.newTableNamed("boleto");
+        eoBoletoTable.newIntBooleanColumn("aceite", false);
+        eoBoletoTable.newStringColumn("banco", 50, true);
+        eoBoletoTable.newDateColumn("data_documento", true);
+        eoBoletoTable.newDateColumn("data_processamento", true);
+        eoBoletoTable.newDateColumn("data_vencimento", true);
+        eoBoletoTable.newIntegerColumn("emissor_id", false);
+        eoBoletoTable.newStringColumn("especie_documento", 16, true);
+        eoBoletoTable.newIntegerColumn("id", false);
+        eoBoletoTable.newStringColumn("numero_documento", 16, true);
+        eoBoletoTable.newBigDecimalColumn("quantidade_moeda", 30, 2, true);
+        eoBoletoTable.newIntegerColumn("sacado_id", false);
+        eoBoletoTable.newBigDecimalColumn("valor", 30, 2, true);
+        eoBoletoTable.newBigDecimalColumn("valor_moeda", 30, 2, true);
+        eoBoletoTable.create();
+        eoBoletoTable.setPrimaryKey("id");
 
-		ERXMigrationTable emissorTable = database.newTableNamed("boleto_emissor");
+        ERXMigrationTable eoEmissorTable = database.newTableNamed("emissor");
+        eoEmissorTable.newIntegerColumn("agencia", true);
+        eoEmissorTable.newIntegerColumn("carteira", true);
+        eoEmissorTable.newStringColumn("cedente", 255, true);
+        eoEmissorTable.newIntegerColumn("codigo_fornecido_pela_agencia", true);
+        eoEmissorTable.newIntegerColumn("codigo_operacao", true);
+        eoEmissorTable.newBigIntegerColumn("conta_corrente", true);
+        eoEmissorTable.newStringColumn("digito_verificador_agencia", 1, true);
+        eoEmissorTable.newStringColumn("digito_verificador_conta_corrente", 1, true);
+        eoEmissorTable.newStringColumn("digito_verificador_nosso_numero", 1, true);
+        eoEmissorTable.newIntegerColumn("id", false);
+        eoEmissorTable.newBigIntegerColumn("nosso_numero", true);
+        eoEmissorTable.newBigIntegerColumn("numero_convenio", true);
+        eoEmissorTable.create();
+        eoEmissorTable.setPrimaryKey("id");
 
-		emissorTable.newIntegerColumn("agencia", true);
-		emissorTable.newIntegerColumn("carteira", true);
-		emissorTable.newStringColumn("cedente", 255, true);
-		emissorTable.newIntegerColumn("codigo_fornecido_pela_agencia", true);
-		emissorTable.newIntegerColumn("codigo_operacao", true);
-		emissorTable.newBigIntegerColumn("conta_corrente", true);
-		emissorTable.newStringColumn("digito_verificador_agencia", 1, true);
-		emissorTable.newStringColumn("digito_verificador_conta_corrente", 1, true);
-		emissorTable.newStringColumn("digito_verificador_nosso_numero", 1, true);
-		emissorTable.newIntegerColumn("id", false);
-		emissorTable.newBigIntegerColumn("nosso_numero", true);
-		emissorTable.newBigIntegerColumn("numero_convenio", true);
-		emissorTable.create();
-		emissorTable.setPrimaryKey("id");
+        ERXMigrationTable eoRequisicaoTable = database.newTableNamed("requisicao");
+        eoRequisicaoTable.newIntegerColumn("boleto_id", false);
+        eoRequisicaoTable.newIntegerColumn("id", false);
+        eoRequisicaoTable.newIntegerColumn("sequential", false);
+        eoRequisicaoTable.create();
+        eoRequisicaoTable.setPrimaryKey("id");
 
-		ERXMigrationTable boletoTable = database.newTableNamed("boleto");
+        ERXMigrationTable eoSacadoTable = database.newTableNamed("sacado");
+        eoSacadoTable.newStringColumn("bairro", 255, true);
+        eoSacadoTable.newStringColumn("cep", 50, true);
+        eoSacadoTable.newStringColumn("cidade", 255, true);
+        eoSacadoTable.newStringColumn("cpf", 50, true);
+        eoSacadoTable.newStringColumn("endereco", 255, true);
+        eoSacadoTable.newIntegerColumn("id", false);
+        eoSacadoTable.newStringColumn("nome", 255, true);
+        eoSacadoTable.newStringColumn("uf", 2, true);
+        eoSacadoTable.create();
+        eoSacadoTable.setPrimaryKey("id");
 
-		boletoTable.newIntBooleanColumn("aceite", false);
-		boletoTable.newStringColumn("banco", 50, false);
-		boletoTable.newTimestampColumn("data_documento", true);
-		boletoTable.newTimestampColumn("data_processamento", true);
-		boletoTable.newTimestampColumn("data_vencimento", true);
-		boletoTable.newIntegerColumn("emissor_id", false);
-		boletoTable.newStringColumn("especie_documento", 16, true);
-		boletoTable.newIntegerColumn("id", false);
-		boletoTable.newStringColumn("numero_documento", 16, true);
-		boletoTable.newBigDecimalColumn("quantidade_moeda", 30, 2, true);
-		boletoTable.newIntegerColumn("sacado_id", false);
-		boletoTable.newBigDecimalColumn("valor", 30, 2, true);
-		boletoTable.newBigDecimalColumn("valor_moeda", 30, 2, true);
-		boletoTable.create();
-		boletoTable.setPrimaryKey("id");
-
-		ERXMigrationTable informacaoTable = database.newTableNamed("boleto_informacao");
-
-		informacaoTable.newIntegerColumn("boleto_id", false);
-		informacaoTable.newIntegerColumn("id", false);
-		informacaoTable.newStringColumn("tipo", 1, false);
-		informacaoTable.newStringColumn("valor", 255, false);
-		informacaoTable.create();
-		informacaoTable.setPrimaryKey("id");
-		
-		ERXMigrationTable eoRequisicaoTable = database.newTableNamed("boleto_eo_requisicao");
-		eoRequisicaoTable.newIntegerColumn("boleto_id", false);
-		eoRequisicaoTable.newIntegerColumn("id", false);
-		eoRequisicaoTable.newIntegerColumn("sequential", false);
-		eoRequisicaoTable.create();
-	 	eoRequisicaoTable.setPrimaryKey("id");
-
-		eoRequisicaoTable.addForeignKey("boleto_id", "boleto", "id");
-
-
-		// boletoTable.addForeignKey( "emissor_id", "boleto_emissor", "id" );
-		// boletoTable.addForeignKey( "sacado_id", "boleto_sacado", "id" );
-	}
+        eoBoletoTable.addForeignKey("emissor_id", "emissor", "id");
+        eoBoletoTable.addForeignKey("sacado_id", "sacado", "id");
+        eoRequisicaoTable.addForeignKey("boleto_id", "boleto", "id");
+    }
 }
