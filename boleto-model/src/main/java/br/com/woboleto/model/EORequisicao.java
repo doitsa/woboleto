fae9a3af0debd0b30188006f243fb7f27e2925c1
Rodrigo Kill <rdskill91@gmail.com>
2014-02-21 11:42:40 -0300

    Adicionar lógica ao salvar a Requisição

--- boleto-model/src/main/java/br/com/woboleto/model/EORequisicao.java
+++ boleto-model/src/main/java/br/com/woboleto/model/EORequisicao.java
@@ -1,5 +1,22 @@
 package br.com.woboleto.model;
 
+import er.extensions.eof.ERXEOControlUtilities;
 
 public class EORequisicao extends _EORequisicao {
+	@Override
+	public void willInsert() {
+		super.willInsert();
+
+		Integer resultado = maiorSequencial();
+		
+		if (resultado == null) {
+			resultado = 0;
+		}
+		
+		setSequential(resultado + 1);
+	}
+
+	Integer maiorSequencial() {
+		return (Integer) ERXEOControlUtilities.aggregateFunctionWithQualifier(editingContext(), ENTITY_NAME, SEQUENTIAL_KEY, "MAX", null);
+	}
 }
---
+++ boleto-model/src/test/java/br/com/woboleto/model/TestEORequisicao.java
@@ -0,0 +1,59 @@
+package br.com.woboleto.model;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.wounit.annotations.Dummy;
+import com.wounit.annotations.UnderTest;
+import com.wounit.rules.MockEditingContext;
+
+/**
+ * TODO: setar sequencial quando nova requisicao for salva
+ * 
+ * @author rdskill
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class TestEORequisicao {
+	@Rule
+	public MockEditingContext mockEditingContext = new MockEditingContext(
+			"Boleto");
+
+	@Spy
+	@UnderTest
+	private EORequisicao requisicao;
+
+	@Dummy
+	private EOBoleto boleto;
+
+	@Test
+	public void setarSequencialInicialQuandoPrimeiraRequisicaoForSalva()
+			throws Exception {
+		requisicao.setBoleto(boleto);
+
+		doReturn(null).when(requisicao).maiorSequencial();
+		
+		mockEditingContext.saveChanges();
+
+		assertThat(requisicao.sequential(), is(1));
+	}
+
+	@Test
+	public void setarSequencialQuandoRequisicaoForSalva() throws Exception {
+		requisicao.setBoleto(boleto);
+		
+		doReturn(20).when(requisicao).maiorSequencial();
+		
+		mockEditingContext.saveChanges();
+		
+		assertThat(requisicao.sequential(), is(21));
+	}
+
+}
