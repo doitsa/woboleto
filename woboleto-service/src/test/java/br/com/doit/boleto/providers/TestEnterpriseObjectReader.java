a0b1d43ed5d0dc86973ee992f12c4b7804de54b8
Henrique Prange <hprange@gmail.com>
2014-03-23 18:56:36 -0300

    Atualiza WOUnit e remove a configuração da factory de editing contexts nos testes

--- woboleto-service/src/test/java/br/com/doit/boleto/providers/TestEnterpriseObjectReader.java
+++ woboleto-service/src/test/java/br/com/doit/boleto/providers/TestEnterpriseObjectReader.java
@@ -148,54 +148,6 @@ public class TestEnterpriseObjectReader {
 	
 	@Before
 	public void setup() {
-		// TODO: Utilizar factory do WOUnit quando ela existir
-		ERXEC.setFactory(new Factory() {
-			public boolean useSharedEditingContext() {
-				return false;
-			}
-
-			public void setUseSharedEditingContext(boolean value) {
-			}
-
-			public void setDefaultNoValidationDelegate(Object delegate) {
-			}
-
-			public void setDefaultEditingContextDelegate(Object delegate) {
-			}
-
-			public void setDefaultDelegateOnEditingContext(EOEditingContext ec,
-					boolean validation) {
-			}
-
-			public void setDefaultDelegateOnEditingContext(EOEditingContext ec) {
-			}
-
-			public Object defaultNoValidationDelegate() {
-				return null;
-			}
-
-			public Object defaultEditingContextDelegate() {
-				return null;
-			}
-
-			public EOEditingContext _newEditingContext(
-					EOObjectStore objectStore, boolean validationEnabled) {
-				return null;
-			}
-
-			public EOEditingContext _newEditingContext(EOObjectStore objectStore) {
-				return null;
-			}
-
-			public EOEditingContext _newEditingContext(boolean validationEnabled) {
-				return null;
-			}
-
-			public EOEditingContext _newEditingContext() {
-				return editingContext;
-			}
-		});
-
 		reader = new EnterpriseObjectReader<EOBoleto>();
 	}
 }
